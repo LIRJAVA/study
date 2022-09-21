7d97d0116b854b8d260c57e3db7fd3367d743dd5
LIR <770560203@qq.com>
2022-09-21 20:48:51 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author ruiL
+ * @create 2022-09-21 20:44
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
